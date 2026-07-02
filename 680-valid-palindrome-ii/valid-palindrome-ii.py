69f2988c6228495c908ea738f7809425a671d136
Column <abc2235154321@gmail.com>
2026-07-02 17:14:42 +0800

    Time: 53 ms (68.74%) | Memory: 19.7 MB (16.00%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    def validPalindrome(self, s: str) -> bool:
+        def check(left,right):
+            while right>left:
+                if s[left]!=s[right]:
+                    return False
+                left+=1
+                right-=1
+            return True
+        left=0
+        right=len(s)-1
+        while right>left:
+            if s[left]!=s[right]:
+                return check(left+1,right) or check(left,right-1)
+            left+=1
+            right-=1
+        return True
